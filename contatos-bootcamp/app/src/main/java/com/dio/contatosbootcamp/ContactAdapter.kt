24b3fc60573172b83f1261ee892d81c7ef866d21
lucas <lucaslcslcs1998@gmail.com>
2022-07-28 08:47:54 -0300

    Implementando a classe ViewHolder

--- contatos-bootcamp/app/src/main/java/com/dio/contatosbootcamp/ContactAdapter.kt
+++ contatos-bootcamp/app/src/main/java/com/dio/contatosbootcamp/ContactAdapter.kt
@@ -1,13 +1,17 @@
 package com.dio.contatosbootcamp
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 import java.text.FieldPosition
 
 class ContactAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapter<ContactAdapter.ViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_view, parent, false)
+        return ViewHolder(view)
     }
     override  fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
         holder.bindItem(contactsList[position])
@@ -16,4 +20,17 @@ class ContactAdapter(val contactsList: ArrayList<Contact>): RecyclerView.Adapter
     override fun getItemCount(): Int {
         return contactsList.size
     }
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+        fun bindItem(contact: Contact) {
+
+            val textName = itemView.findViewById<TextView>(R.id.contact_name)
+            val textPhone = itemView.findViewById<TextView>(R.id.phone_number)
+
+            textName.text = contact.name
+            textPhone.text = contact.phoneNumber
+        }
+
+    }
 }
+
